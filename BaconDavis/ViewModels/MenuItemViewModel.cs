9ea70fd54b018c303d1f973b50dfa65a14c6047d
jlangfordcsman <jason.langford.billings@gmail.com>
2015-03-29 00:10:34 -0600

    added employee module / various styling

---
+++ BaconDavis/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaconDavis.ViewModels
+{
+    public class MenuItemViewModel
+    {
+        private IRegionManager regionManager;
+
+        public MenuItemViewModel(IRegionManager regionManager)
+        {
+            NavigateCommand = new DelegateCommand(Navigate);
+            this.regionManager = regionManager;
+        }
+
+        public DelegateCommand NavigateCommand { get; set; }
+
+        private void Navigate()
+        {
+            this.regionManager.RequestNavigate("MainRegion", typeof(Views.MainView).FullName);
+        }
+    }
+}
---
+++ Employees/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        
+    }
+}
